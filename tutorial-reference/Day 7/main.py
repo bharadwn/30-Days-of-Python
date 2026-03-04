788247f0cb9db9e560dac8d8727657e42c40903e
bharadwn <bharadwn@yahoo.com>
2026-03-04 13:47:40 -0800

    Update main.py

--- tutorial-reference/Day 7/main.py
+++ tutorial-reference/Day 7/main.py
@@ -5,11 +5,11 @@ def my_print(txt):
 msg_template = """Hello {name},
 Thank you for joining {website}. We are very
 happy to have you with us.
-""" # .format(name="Justin", website='cfe.sh')
+""" .format(name="Justin", website='cfe.sh')
 
 def format_msg(my_name="Justin", my_website="cfe.sh"):
     my_msg = msg_template.format(name=my_name, website=my_website)
-    # print(my_msg)
+    print(my_msg)
     return my_msg
 
 """
